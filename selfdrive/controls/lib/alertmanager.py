0c79ea82c9e9b1a90f2f366d4a025d3354db3062
Laurens Wuyts <laurens.wuyts@gmail.com>
2018-05-17 11:13:04 +0200

    Edit Test

--- selfdrive/controls/lib/alertmanager.py
+++ selfdrive/controls/lib/alertmanager.py
@@ -102,7 +102,7 @@ class AlertManager(object):
 
     "startup": Alert(
         "Be ready to take over at any time",
-        "Always keep hands on wheel and eyes on road",
+        "Always keep hands on wheel and eyes on road :P",
         AlertStatus.normal, AlertSize.mid,
         Priority.LOWEST, None, None, 0., 0., 15.),
 
